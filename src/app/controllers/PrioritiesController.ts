c991c12d7aaf289d9541592881dbf73b81f2f4e8
Ichiroki <fahrezirizqiawan12649@gmail.com>
2024-08-24 20:54:51 +0700

    Implement: Add update & delete priorities

--- src/app/controllers/PrioritiesController.ts
+++ src/app/controllers/PrioritiesController.ts
@@ -19,3 +19,44 @@ export const addPriorities = async (req, res) => {
         console.error('Internal Server Error, please wait : ' + e)
     }
 }
+
+export const updatePriorities = async (req, res) => {
+    const id = req.params['priority_id']
+
+    try {
+        const priorities = await prisma.priorities.updateMany({
+            where:{
+                id
+            },
+            data: {
+                priorities_name: req.body.priorities_name,
+            }
+        })
+        if(priorities) {
+            res.status(201).json({success: true, message: 'New priority has been added'})
+        } else {
+            res.status(422).json({success: true, message: 'Add new priority failed'})
+        }
+    } catch(e) {
+        console.error('Internal Server Error, please wait : ' + e)
+    }
+}
+
+export const deletePriorities = async (req, res) => {
+    const id = req.params['priority_id']
+
+    try {
+        const priorities = await prisma.priorities.delete({
+            where: {
+                id
+            },
+        })
+        if(priorities) {
+            res.status(201).json({success: true, message: 'New priority has been added'})
+        } else {
+            res.status(422).json({success: true, message: 'Add new priority failed'})
+        }
+    } catch(e) {
+        console.error('Internal Server Error, please wait : ' + e)
+    }
+}
